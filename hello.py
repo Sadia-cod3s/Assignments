c8c7d98a860e3dcd78a24eaba5bda4fad7bb3d1d
Sadia-cod3s <bestpostbusiness@gmail.com>
2026-06-30 01:08:24 +0500

    Assignment 1

---
+++ hello.py
@@ -0,0 +1,10 @@
+name ="sadia"
+print("My name is:" , name)
+color = "dark green"
+print("My fav color is:" , color)
+hobby = "reading"
+print("My fav hobby is:" , hobby)
+age = "14"
+print("My age is:" , age)
+country = "Pakistan"
+print("My country is:" , country)
