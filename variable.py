974f48f5d0cd6225724293abfcd46d6e6be6068b
Sadia-cod3s <bestpostbusiness@gmail.com>
2026-06-30 17:21:04 +0500

    Assignment 2

---
+++ variable.py
@@ -0,0 +1,15 @@
+name = "Sadia Umair"
+print("my name is" , name)
+print(type(name))
+
+age = "14"
+print("my age is" , age)
+print(type(age))
+
+country = "Pakistan"
+print("my country is" , country)
+print(type(country))
+
+student_class = "10"
+print("i am in class" , student_class)
+print(type()) 
